fd5779adf1d20cf7bd6bbc2138ba8d52e1385439
alexchonglian <alexchonglian@gmail.com>
2019-05-28 16:32:06 -0700

    09

--- ud401-ga/08v03-04CM.py
+++ ud401-ga/08v03-04CM.py
@@ -1,13 +1,13 @@
 # Complex multiplication
 
 def cm0(a,b,c,d):
-	return a*c-b*d, b*c+a*d
+    return a*c-b*d, b*c+a*d
 
 def cm1(a,b,c,d):
-	ac, bd, tsum = a*c, b*d, (a+b)*(c+d)
-	return ac-bd, tsum-ac-bd
+    ac, bd, tsum = a*c, b*d, (a+b)*(c+d)
+    return ac-bd, tsum-ac-bd
 
 if __name__ == '__main__':
-	print(cm1(0,1,0,1))
-	print(cm1(4,3,-4,3))
-	print(cm1(3,4,-4,-3))
+    print(cm1(0,1,0,1))
+    print(cm1(4,3,-4,3))
+    print(cm1(3,4,-4,-3))
--- ud401-ga/08v05-08FM.py
+++ ud401-ga/08v05-08FM.py
@@ -1,3 +1,8 @@
+from time import time
+
+def fm(x,y):
+    return x*y
+
 def fm0(x, y, n):
     # input: n-bit integers x & y, n = 2^k
     # output: z = x*y
@@ -23,6 +28,7 @@ def fm2(x,y,count=0):
     # input: x (m-bit int), y (n-bit int)
     # output: z = x*y
     # compute bit length for left half and right half?
+    # enable full debug?
     def log(msg):
         return
         print(" "*2*count+msg)
@@ -45,7 +51,40 @@ def fm2(x,y,count=0):
     log("z=%s"%z)
     return z
 
-# reminds me of another way to multiply
+
+def fm3(x,y,count=0):
+    # input: x (m-bit int), y (n-bit int)
+    # output: z = x*y
+    # time: T(n) = 4T(n/2) + O(n)
+    xlen, ylen = x.bit_length(), y.bit_length()
+    n = max(xlen,ylen)
+    if min(xlen,ylen) <= 1:
+        return x*y
+    shift = n//2
+    mask = (1 << shift) - 1
+    xl,xr,yl,yr = x>>shift, x&mask, y>>shift, y&mask
+    a,b,c,d = fm3(xl,yl,count+1), fm3(xr,yr,count+1), fm3(xl,yr,count+1), fm3(xr,yl,count+1)
+    z = (1<<shift*2)*a + (1<<shift)*(c+d) + b
+    return z
+
+def fm4(x,y,count=0):
+    # Improved Fast Multiplication
+    # input: x (m-bit int), y (n-bit int)
+    # output: z = x*y
+    # time: T(n) = 3T(n/2) + O(n)
+    xlen, ylen = x.bit_length(), y.bit_length()
+    n = max(xlen,ylen)
+    if min(xlen,ylen) <= 1:
+        return x*y
+    shift = n//2
+    mask = (1 << shift) - 1
+    xl,xr,yl,yr = x>>shift, x&mask, y>>shift, y&mask
+    a,b,c = fm4(xl,yl,count+1), fm4(xr,yr,count+1), fm4(xl+xr,yl+yr,count+1)
+    z = (1<<shift*2)*a + (1<<shift)*(c-a-b) + b
+    return z
+
+# not taught in ud401 but
+# it reminds me of another way to multiply
 def mul0(x, y):
     z = 0
     while y > 0:
@@ -61,23 +100,36 @@ def mul1(x, y):
         y = y >> 1
     return z 
 
-def test_mf2():
-    assert fm2(50, 3) == 150
-    assert fm2(12, 3) == 36
-    assert fm2(10, 3) == 30
-    assert fm2(4, 5) == 20
-    assert fm2(20, 5) == 100
-    assert fm2(17, 5) == 85
-    assert fm2(9, 3) == 27
-    assert fm2(1000, 10) == 10000
+def run_tests(name):
+    f = globals().get(name)
+    assert f(50, 3) == 150
+    assert f(12, 3) == 36
+    assert f(10, 3) == 30
+    assert f(4, 5) == 20
+    assert f(20, 5) == 100
+    assert f(17, 5) == 85
+    assert f(9, 3) == 27
+    assert f(1000, 10) == 10000
+
+def timed_exec(name):
+    f = globals().get(name)
+    start = time()
+    for i in range(100):
+        f(29642949672949672964, 29496729642964294967)
+    end = time()
+    print("%s takes %f" % (name, end-start))
 
 if __name__ == '__main__':
-    test_mf2()
-    print(2964294967*2949672964)
-    print(fm0(2964294967, 2949672964, 32))
-    print(fm1(2964294967, 2949672964))
-    print(fm1(1000, 10))
-    print(fm2(2964294967, 2949672964))
-    print(fm2(1000, 10))
+    # this is so stupid
+    timed_exec('fm') # fm takes 0.000021
+    timed_exec('fm3') # fm3 takes 0.228150
+    timed_exec('fm4') # fm4 takes 0.153585
+
+    #print(2964294967*2949672964)
+    #print(fm0(2964294967, 2949672964, 32))
+    #print(fm1(2964294967, 2949672964))
+    #print(fm1(1000, 10))
+    #print(fm2(2964294967, 2949672964))
+    #print(fm2(1000, 10))
     #print(mul0(2964294967, 2949672964)) # wont finish
-    print(mul1(2964294967, 2949672964))
+    #print(mul1(2964294967, 2949672964))
